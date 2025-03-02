c56333c37dfee59317de88b70b4750b44fa53d3f
Kushagra Anand <134623436+Kushagra-maker@users.noreply.github.com>
2025-03-02 18:57:07 +0530

    Update explicittypeconversion.js

--- Jan_14/explicittypeconversion.js
+++ Jan_14/explicittypeconversion.js
@@ -1,2 +1,19 @@
 console.log(Boolean(""));
 console.log(String(123));
+
+console.log(Number("123"));
+console.log(Number("abcd"));
+console.log(Number("0xa"));
+
+let x = -0;
+console.log(x === NaN);
+console.log(isNaN(x));
+
+console.log(isNaN("sanket")); //isNaN converts the incoming input to a number (ToNumber)
+
+console.log(Number.isNaN("sanket"));
+console.log(Number.isNaN(x));
+
+if(typeof(x) === number && x !== x){
+    console.log(true);
+}
