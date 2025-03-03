f6db26ea360591c4a5103cb9c0704c8e9dd49f23
Kushagra Anand <134623436+Kushagra-maker@users.noreply.github.com>
2025-03-03 15:08:49 +0530

    Update coercionexamples.js

--- Jan_14/coercionexamples.js
+++ Jan_14/coercionexamples.js
@@ -1,4 +1,5 @@
 let obj = {x: 10, y: 20};
-let num = 10;
-console.log(`My object is ${obj}`);
-console.log("My object is"+obj);
+let num = 10; 
+console.log(`My object is ${obj}`); // My object is [object Object] // When you use template literals with an object in JavaScript, the default behavior is to convert the object to a string using its toString() method.
+console.log("My object is"+obj); // My object is  [object Object]
+console.log(`My object is ${num}`); // My object is 20
